763b52004a16a86e5aac875672c63aaa1c364864
Rob Hill <robert.hill0122@gmail.com>
2014-07-09 09:29:13 -0600

    completed 3rd exercise

--- lib/dog_hash.rb
+++ lib/dog_hash.rb
@@ -1,10 +1,11 @@
 DOG = {
   cattle_dog: {
     preferences: [
-      name: "Harleigh"
+      name: "Harleigh", favorite_toy: "frog"
     ],
     names: ["Harleigh", "brown"]
-  }
+  },
+  "harleigh" => {preferences: [favorite_toy: "frog"]}
 }
 
 
